5f83ef3bda2de462e121e95ecf14d73aa85316ce
scirelli <scirelli+git@gmail.com>
2015-01-26 06:49:07 -0500

    Adding phone number parser

--- regex.js
+++ regex.js
@@ -26,5 +26,52 @@ module.exports = {
         ,'ig'),
         //http://stackoverflow.com/questions/8188645/javascript-regex-to-match-a-url-in-a-field-of-text
         //var oRegEx = new RegExp("(http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?");
-    regURLSimple:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i
+    regURLSimple:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i,
+    PhoneNumberParser:function(){
+        var minimum = 9;            // typical minimum phone number length
+        this.items = [];
+
+        var public = PhoneNumberParser.prototype;
+        public.parse = function(str) {
+            var items = this.items = [];
+
+            var i = 0, n = '', min = minimum;
+
+            while(i < str.length) {
+                switch(str[i]) {
+                case '+':                                   // start of international number
+                    if (n.length >= min) items.push(n);
+                    n = str[i];
+                    min = minimum + 2;                      // at least 2 more chars in number
+                    break;
+                case '-': case '.': case '(': case ')':     // ignore punctuation
+                    break;
+                case ' ':
+                    if (n.length >= min) {              // space after consuming enough digits is end of number
+                        items.push(n);
+                        n = '';
+                    }
+                    break;
+                default:
+                    if (str[i].match(/[0-9]/)) {            // add digit to number
+                        n += str[i];
+                        if (n.length == 1 && n != '0') {
+                            min = 3;                        // local number (extension possibly)
+                        }
+                    } else {
+                        if (n.length >= min) {
+                            items.push(n);                  // else end of number
+                        }
+                        n = '';
+                    }
+                    break;
+                }
+                i++;
+            }
+
+            if (n.length >= min) {              // EOF
+                items.push(n);
+            }
+        }
+    }
 }
